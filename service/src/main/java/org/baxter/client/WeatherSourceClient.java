b3b3486b7040c41c8a8ceef6235fe05dcfcf6fa2
Rory Baxter <rorybaxter99@btinternet.com>
2024-03-03 09:09:35 +0000

    service-make-client: made http client

---
+++ service/src/main/java/org/baxter/client/WeatherSourceClient.java
@@ -0,0 +1,15 @@
+package org.baxter.client;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WeatherSourceClient {
+
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public HttpResponse<String> callAPI(final HttpRequest request) throws IOException, InterruptedException {
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
